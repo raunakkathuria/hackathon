df9c195df0da2b1099e68c479042698479e4d10e
Kirill Sysoev <kirill@binary.com>
2021-05-28 15:46:33 +0800

    Move code to another dir

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,13 @@
-console.log('here');
+class Worker {
+    constructor(redis_host, redis_port) {
+      this.redis_host = redis_host;
+      this.redis_port = redis_port;
+    }
+
+    run(){
+        
+    } 
+}
+
+let worker = new Worker('test', 'test');
+worker.run();
