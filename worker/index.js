fdfe1f8d84329c650ebc53ba9762c397f4b7debe
Kirill Sysoev <kirill@binary.com>
2021-05-28 17:01:08 +0800

    using async await

--- worker/index.js
+++ worker/index.js
@@ -3,14 +3,12 @@ const { promisify } = require("util");
 
 class Worker {
     constructor(redis_url, stream_name) {
-      this.redis = redis.createClient(redis_url);
+      this.redis = redis.createClient();
       this.stream_name = stream_name;
     }
     async init () {
-        console.log(1);
         let afunc = promisify(this.redis.xgroup).bind(this.redis);
-        await afunc('CREATE', this.stream_name, 'worker', '$');
-        console.log(2);
+        await afunc('CREATE', 'test', 'worker', '$', 'MKSTREAM');
     }
     async run(){
         //init connection
