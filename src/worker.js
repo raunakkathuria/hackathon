9d0b09b11a5c9e6e9a52aeb68f22e502ccf47789
Kirill Sysoev <kirill@binary.com>
2021-05-28 15:29:39 +0800

    Worker place holder

---
+++ src/worker.js
@@ -0,0 +1,10 @@
+class Rectangle {
+    constructor(redis_host, redis_port) {
+      this.redis_host = redis_host;
+      this.redis_port = redis_port;
+    }
+
+    run(){
+        console.log("test run");
+    } 
+}
